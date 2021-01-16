1d2e7e0097b4f9c40c3bc44235a8e407465707c5
Filbert Cornelius <72387173+Phillsxu@users.noreply.github.com>
2021-01-16 23:25:44 +0800

    Add files via upload

---
+++ Pods/Target Support Files/Pods-Black Eyed Peas/Pods-Black Eyed Peas-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Black_Eyed_Peas : NSObject
+@end
+@implementation PodsDummy_Pods_Black_Eyed_Peas
+@end
---
+++ Pods/Target Support Files/Pods-Black Eyed PeasTests/Pods-Black Eyed PeasTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Black_Eyed_PeasTests : NSObject
+@end
+@implementation PodsDummy_Pods_Black_Eyed_PeasTests
+@end
